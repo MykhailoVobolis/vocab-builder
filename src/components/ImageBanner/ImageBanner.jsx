cadb26d73e090fe57f192bb83930244ed2705967
MykhailoVobolis <mvobolis@gmail.com>
2024-11-15 16:21:06 +0200

    added tablet styles

--- src/components/ImageBanner/ImageBanner.jsx
+++ src/components/ImageBanner/ImageBanner.jsx
@@ -8,13 +8,14 @@ import css from "./ImageBanner.module.css";
 export default function ImageBanner() {
   const { pathname } = useLocation();
   const isMobile = useMedia("(max-width: 767px)");
+  const isDesktop = useMedia("(min-width: 1440px)");
   const isRegister = pathname.includes("register");
 
   const visible = !(isMobile && isRegister);
 
   return (
     <div className={css.container}>
-      <MainPicture />
+      {(isMobile || isDesktop) && <MainPicture />}
       {visible && (
         <ul className={css.descList}>
           <li className={css.afterElement}>Word</li>
